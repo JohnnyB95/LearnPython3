a1691cb434a2f3538b8b148bf8e6af65d8bd8318
Johnny Benitez <Johnny.Benitez@gmail.com>
2018-12-20 14:33:44 +0800

    Initial commit

---
+++ ex3.py
@@ -0,0 +1,29 @@
+# Print line
+print ("I will now count my chickens:")
+
+# Inline calculation
+print ("Hens", 25 + 30 / 6)
+print ("Roosters", 100 - 25 * 3 % 4)
+
+# Print intro line
+# Print straight calculation using PEMDAS
+print ("Now I will count the eggs:")
+
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Print with boolean evaluation
+print("Is it true that 3 + 2 < 5 -7?")
+
+print (3 + 2 < 5 - 7)
+
+print ("What is 3 + 2?", 3 + 2)
+print ("What is 5 - 7?", 5 - 7)
+
+print ("Oh, that's why its False.")
+
+print ("How about some more.")
+
+# Print with Booelan comparison
+print ("Is it greater?", 5 > -2)
+print ("Is it greater or equal?", 5 >= -2)
+print ("Is it less or equal?", 5 <= -2)
