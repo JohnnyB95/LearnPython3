aede233a55b60d06f944f0a4241b436480fbbef8
Johnny Benitez <Johnny.Benitez@gmail.com>
2018-12-24 01:13:31 +0800

    New exercises as of Dec 23, 2018

---
+++ ex15.py
@@ -0,0 +1,21 @@
+#This exercise is for opening and reading files
+#Dependent on ex15_sample.txt as input
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print (f"Here's your file {filename}:")
+print (txt.read())
+
+print ("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print (txt_again.read())
+
+txt.close()
+txt_again.close()
